dbca19b0cc1f61b2c9884b12941a20392b0dada9
jjbaldizon16 <baldizon.cv16@gmail.com>
2021-07-21 05:37:39 -0600

    Unidad 7 Capitulo: Temas Hijo.
    Unidad 7 Capitulo: Temas Hijo.
    Minuto:(0:00)

--- inc/scripts.php
+++ inc/scripts.php
@@ -5,11 +5,18 @@
  */
 function domestika_enqueue_scripts() {
 	wp_enqueue_style( 'domestika-style', get_stylesheet_uri() );
+
 	if ( is_single() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 
-	wp_enqueue_script( 'comment-reply' );
-
+	if ( is_post_type_archive( 'proyecto' ) || is_tax( 'tipo_de_proyecto' ) ) {
+		wp_enqueue_script( 'scrollify', get_template_directory_uri() . '/assets/js/scrollify.js', [ 'jquery' ] );
+		wp_enqueue_script( 'domestika-init', get_template_directory_uri() . '/assets/js/init.js', [ 'jquery', 'scrollify' ] );
 	}
 
 }
 add_action( 'wp_enqueue_scripts', 'domestika_enqueue_scripts' );
+
+
+
